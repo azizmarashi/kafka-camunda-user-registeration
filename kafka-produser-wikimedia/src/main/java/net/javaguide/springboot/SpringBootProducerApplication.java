df811fd4e9ddf67ab836d3488818770079ae6103
Aziz Hosseini <shosseini@payeshgaran.net>
2023-04-04 14:05:49 +0330

    commit

--- kafka-produser-wikimedia/src/main/java/net/javaguide/springboot/SpringBootProducerApplication.java
+++ kafka-produser-wikimedia/src/main/java/net/javaguide/springboot/SpringBootProducerApplication.java
@@ -1,11 +1,19 @@
 package net.javaguide.springboot;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 
 @SpringBootConfiguration
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    private WikimediaChangesProducer wikimediaChangesProducer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikimediaChangesProducer.sendMessage();
     }
 }
